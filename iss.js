e8c9de1e5a3ea51cb4089386ec40df723f71f9da
sfia-o <sofia.vl.oliveira@gmail.com>
2023-06-15 03:54:18 +0000

    Refactored and converted IP into string using JSON.parse

--- iss.js
+++ iss.js
@@ -11,17 +11,17 @@ const request = require('request');
 const fetchMyIP = function(callback) {
 
   request('https://api.ipify.org/?format=json', (error, response, body) => {
-    if(error) {
-      callbak(error, null);
-      return;
-    };
+    if (error) {
+      return callback(error, null);
+    }
 
-    if(response.statusCode !== 200) {
-      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
-      callback(Error(msg), null);
+    if (response.statusCode !== 200) {
+      callback(Error(`Status Code ${response.statusCode} when fetching IP. Response: ${body}`), null);
       return;
     }
-    callback(null, body);
+
+    const ip = JSON.parse(body).ip;
+    callback(null, ip);
   });
 
 };
