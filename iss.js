ee02e2f3fde95e08f8d73a6c0e7fff1e522cbea4
sfia-o <sofia.vl.oliveira@gmail.com>
2023-06-15 03:45:15 +0000

    Added request function with callback

--- iss.js
+++ iss.js
@@ -6,8 +6,17 @@
  *   - An error, if any (nullable)
  *   - The IP address as a string (null if error). Example: "162.245.144.188"
  */
+const request = require('request');
 
 const fetchMyIP = function(callback) {
+
+  request('https://api.ipify.org/?format=json', (error, response, body) => {
+    if(error) {
+      callbak(error, null);
+      return;
+    };
+    callback(null, body);
+  });
 
 };
 
