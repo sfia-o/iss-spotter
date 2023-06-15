a39c9a1b3246af8374369e750f495ae7cda22a14
sfia-o <sofia.vl.oliveira@gmail.com>
2023-06-15 17:30:14 +0000

    Deleted commented functions

--- index.js
+++ index.js
@@ -1,33 +1,5 @@
 const { nextISSTimesForMyLocation } = require('./iss');
 
-
-// fetchMyIP((error, ip) => {
-//   if (error) {
-//     console.log('There was an error', error);
-//     return;
-//   }
-
-//   console.log('IP is: ', ip);
-// });
-
-// fetchCoordsByIP('173.181.51.41', (error, coordinates) => {
-//   if (error) {
-//     console.log("It didn't work!", error);
-//     return;
-//   }
-
-//   console.log(`Here are the IP's coordinates: `, coordinates);
-// });
-
-// fetchISSFlyOverTimes({latitude: 48.4284207, longitude: -123.3656444}, (error, coords) => {
-//   if (error) {
-//     console.log(("it didn't work!", error));
-//     return;
-//   }
-
-//   console.log(coords);
-// });
-
 const print = function(passTimes) {
 
   for (const time of passTimes) {
